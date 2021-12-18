b41f48e613f1d1f2bef7b90b9321afea1e45971e
maxild <mmaxild@gmail.com>
2021-12-18 10:16:32 +0100

    Updated to .NET 6

--- Module01-Introduction/Benchmark/Fibonacci.cs
+++ Module01-Introduction/Benchmark/Fibonacci.cs
@@ -1,45 +1,42 @@
-﻿using System.Collections.Generic;
-using BenchmarkDotNet.Attributes;
+﻿using BenchmarkDotNet.Attributes;
 
-namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
+namespace Benchmark;
+
+[DisassemblyDiagnoser(exportCombinedDisassemblyReport: true)]
+public class FibonacciCalc
 {
-    [DisassemblyDiagnoser(exportCombinedDisassemblyReport: true)]
-    public class FibonacciCalc
+    // HOMEWORK:
+    // 1. Write implementations for RecursiveWithMemoization and Iterative solutions
+    // 2. Add MemoryDiagnoser to the benchmark
+    // 3. Run with release configuration and compare results
+    // 4. Open disassembler report and compare machine code
+    //
+    // You can use the discussion panel to compare your results with other students
+
+    [Benchmark(Baseline = true)]
+    [ArgumentsSource(nameof(Data))]
+    public ulong Recursive(ulong n)
     {
-        // HOMEWORK:
-        // 1. Write implementations for RecursiveWithMemoization and Iterative solutions
-        // 2. Add MemoryDiagnoser to the benchmark
-        // 3. Run with release configuration and compare results
-        // 4. Open disassembler report and compare machine code
-        // 
-        // You can use the discussion panel to compare your results with other students
+        return n is 1 or 2 ? 1 : Recursive(n - 2) + Recursive(n - 1);
+    }
 
-        [Benchmark(Baseline = true)]
-        [ArgumentsSource(nameof(Data))]
-        public ulong Recursive(ulong n)
-        {
-            if (n == 1 || n == 2) return 1;
-            return Recursive(n - 2) + Recursive(n - 1);
-        }
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public ulong RecursiveWithMemoization(ulong n)
+    {
+        return 0;
+    }
 
-        [Benchmark]
-        [ArgumentsSource(nameof(Data))]
-        public ulong RecursiveWithMemoization(ulong n)
-        {
-            return 0;
-        }
-        
-        [Benchmark]
-        [ArgumentsSource(nameof(Data))]
-        public ulong Iterative(ulong n)
-        {
-            return 0;
-        }
+    [Benchmark]
+    [ArgumentsSource(nameof(Data))]
+    public ulong Iterative(ulong n)
+    {
+        return 0;
+    }
 
-        public IEnumerable<ulong> Data()
-        {
-            yield return 15;
-            yield return 35;
-        }
+    public static IEnumerable<ulong> Data()
+    {
+        yield return 15;
+        yield return 35;
     }
 }
--- Module01-Introduction/Benchmark/Program.cs
+++ Module01-Introduction/Benchmark/Program.cs
@@ -1,13 +1,4 @@
 ﻿using System.Reflection;
 using BenchmarkDotNet.Running;
 
-namespace Dotnetos.AsyncExpert.Homework.Module01.Benchmark
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
-        }
-    }
-}
+BenchmarkSwitcher.FromAssembly(Assembly.GetExecutingAssembly()).Run(args);
