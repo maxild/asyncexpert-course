67e9b02ec75c2be4dc5d98684efd6727e67bdf67
Morten Maxild <mmaxild@gmail.com>
2021-12-23 16:54:50 +0100

    Added thread guard demo

--- Module02-AsyncBasics/Experiments/ThreadPoolConsoleApp/Program.cs
+++ Module02-AsyncBasics/Experiments/ThreadPoolConsoleApp/Program.cs
@@ -16,7 +16,18 @@ var spawningThread = new Thread(SpawnWork)
 };
 spawningThread.Start();
 
-Console.ReadLine();
+// fetching the cursor position (Console.CursorTop getter) and Console.Read
+// take the same lock, on Linux
+//      Console.ReadLine();
+// See https://github.com/dotnet/runtime/issues/49301
+// See https://github.com/dotnet/runtime/blob/ec1897724f6dc5ab49eebde5f35292493e1bed7b/src/libraries/System.Console/src/System/ConsolePal.Unix.cs#L470-L475
+Console.CursorTop = 5;
+Console.WriteLine($"Work Item Delay {Constants.SINGLE_WORK_ITEM_DELAYS_MS} ms. Press Ctrl+C to terminate");
+// Cannot use Readline on Linux...so we spin
+while (true)
+{
+    Thread.Sleep(1000);
+}
 
 static void PrintThreadPoolStats()
 {
@@ -44,9 +55,7 @@ static void SpawnWork()
 
     static void DoWork(object? state)
     {
-        // 2_000  - some queuing, because we have 12 logical cores (10 * 2000 per second, we can process  12 * 1000ms per second)
-        // 60_000 - masive queuing, big increase in #worker threads
-        const int SINGLE_WORK_ITEM_DELAYS_MS = 1_000;
+
 
         int cursorLeft = state == null ? 0 : (int)state;
 
@@ -56,15 +65,28 @@ static void SpawnWork()
         // clear current line
         if (cursorLeft == 0)
         {
+            // use the carriage return to go to the beginning of the line, then print
+            // as many spaces as the console is width and returns to the beginning of the
+            // line again
+            Console.Write("\r" + new string(' ', Console.WindowWidth) + "\r");
+
             // Console.SetCursorPosition(0, 3);
-            Console.Write(new string(' ', Console.BufferWidth));
+            // Console.Write(new string(' ', Console.BufferWidth));
             // Console.Write(new string(' ', Console.WindowWidth));
-            Console.SetCursorPosition(0, 3);
+            // In your case, instead of Console.SetCursorPosition(0, Console.CursorTop); try writing a \r.
+            // Console.SetCursorPosition(0, 3);
         }
 
         Console.Write(".");
 
         // How long does the work take (simulate work via sleep)
-        Thread.Sleep(SINGLE_WORK_ITEM_DELAYS_MS);
+        Thread.Sleep(Constants.SINGLE_WORK_ITEM_DELAYS_MS);
     }
+}
+
+static class Constants
+{
+    // 2_000  - some queuing, because we have 12 logical cores (10 * 2000 per second, we can process  12 * 1000ms per second)
+    // 60_000 - masive queuing, big increase in #worker threads
+    public const int SINGLE_WORK_ITEM_DELAYS_MS = 60_000;
 }
---
+++ Module02-AsyncBasics/Experiments/ThreadPoolProcessingGuardConsoleApp/Program.cs
@@ -0,0 +1,87 @@
+﻿#define USE_GUARD
+
+static class Program
+{
+    private static AutoResetEvent s_guardRunningFlag = new(false);
+
+    public static void Main()
+    {
+        // TODO: Maybe spin instead
+        // Console.ReadKey();
+
+        Console.Clear();
+        Console.CursorVisible = false;
+
+        // To make queueing much easier
+        ThreadPool.SetMinThreads(workerThreads: 12, completionPortThreads: 12);
+        // You cannot go lower than these numbers
+        ThreadPool.SetMinThreads(workerThreads: Environment.ProcessorCount, completionPortThreads: 1000);
+
+
+#if USE_GUARD
+        ThreadPool.QueueUserWorkItem(ProcessingGuard);
+        s_guardRunningFlag.WaitOne();
+#endif
+
+        // Enqueue 20 work items
+        var random = new Random();
+        for (int i = 0; i < 20; i++)
+            ThreadPool.QueueUserWorkItem(DoWork, i);
+
+        // App just ends, no waiting here...
+        // Console.ReadKey();
+    }
+
+    static void DoWork(object? state)
+    {
+        var previousState = Thread.CurrentThread.IsBackground ? "B" : "F";
+        // Thread.CurrentThread.IsBackground = false;
+        const int ITER = 10; // 10 times each second
+        int id = (int)state!;
+        Debug(0, 0, $"{id:D2}{previousState}");
+        for (int i = 0; i < ITER; i++)
+        {
+            Debug(i + 5, id + 1, ".");
+            Thread.Sleep(1000);
+        }
+    }
+
+    static void ProcessingGuard(object? state)
+    {
+        // make foreground thread to guard against closing
+        Thread.CurrentThread.IsBackground = false;
+        s_guardRunningFlag.Set();
+
+        const int EmptyQueueThresholdMilliseconds = 3_000;
+        const int LoopDelayMilliseconds = 100;
+
+        int emptyQueueCounter = 0;
+        int emptyQueueThreshold = EmptyQueueThresholdMilliseconds / LoopDelayMilliseconds;
+        while (true)
+        {
+            long queue = ThreadPool.PendingWorkItemCount;
+            Debug(0, 0, $"Guard Q:{queue: D2}, C:{emptyQueueCounter:D2}");
+            if (queue == 0)
+            {
+                // sum up the number of times empty queue is observed
+                emptyQueueCounter += 1;
+                if (emptyQueueCounter >= emptyQueueThreshold)
+                    break; // observed 30 times = 3 seconds
+            }
+            else
+            {
+                // reset the counter
+                emptyQueueCounter = 0;
+            }
+            Thread.Sleep(LoopDelayMilliseconds);
+        }
+        Debug(0, 0, "G: done!");
+    }
+
+    static void Debug(int x, int y, string s)
+    {
+        Console.CursorTop = x;
+        Console.CursorLeft = y;
+        Console.Write(s);
+    }
+}
