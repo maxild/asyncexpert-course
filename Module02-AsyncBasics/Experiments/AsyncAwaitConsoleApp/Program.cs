3e4ab602aabc1f51daff0a7c3dfbf6ca12e4324e
maxild <mmaxild@gmail.com>
2021-12-25 19:19:51 +0100

    demo to decompile lowered C# using async/await

---
+++ Module02-AsyncBasics/Experiments/AsyncAwaitConsoleApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+internal class TrivialAsyncDemo
+{
+    private static async Task Main()
+    {
+        string s = await File.ReadAllTextAsync(@"C:\Users\maxfire\.gitconfig");
+        Console.Write(s);
+    }
+}
